ebedaa7fac90ae9d9d89baab4aa88e9fb32c1503
DmitriiFRS <dmitriifrs@gmail.com>
2026-01-24 09:25:44 +0500

    finished home

--- src/app/[locale]/(home)/_components/About.tsx
+++ src/app/[locale]/(home)/_components/About.tsx
@@ -1,4 +1,4 @@
-import sample from "@/assets/images/homepage/sample.png";
+import sample from "@/assets/images/homepage/about-homepage.webp";
 import Container from "@/shared/ui/Container";
 import Image from "next/image";
 
@@ -7,7 +7,7 @@ const About = () => {
 		<section className="pt-15 md:pt-25">
 			<Container className="md:flex md:gap-10 lg:gap-20">
 				<div className="w-full h-auto aspect-square rounded-[14px] md:aspect-528/327">
-					<Image src={sample} alt="sample" width={528} height={327} className="w-full h-full" />
+					<Image src={sample} alt="sample" width={528} height={276} className="w-full h-full object-cover rounded-[14px]" />
 				</div>
 				<div className="flex flex-col mt-10">
 					<span className="text-grey-primary text-11 leading-118 font-medium tracking-neg-2">[ О компании ]</span>
@@ -20,8 +20,12 @@ const About = () => {
 							рынок.
 						</p>
 						<p>
-							Мы не занимаемся рекламным продакшеном и операционной реализацией. Наша зона ответственности — стратегия, аналитика и
-							экспертные рекомендации, которые становятся фундаментом для роста фармбизнеса.
+							Наша работа не заканчивается презентацией: мы формируем понятный план действий, сопровождаем команды на этапе внедрения
+							и измеряем эффект через KPI — продажи, долю рынка и эффективность инвестиций.
+						</p>
+						<p>
+							Фокус — стратегия, аналитика, продуктовый портфель и go-to-market решения, которые дают практический результат и
+							становятся основой устойчивого роста.
 						</p>
 					</div>
 				</div>
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,8 @@
 import { ContactSection } from "@/widgets/ContactSection";
 import About from "./(home)/_components/About";
 import Advantages from "./(home)/_components/Advantages";
-import Advantages2 from "./(home)/_components/Advantages2";
-import Carousel from "./(home)/_components/Carousel";
+// import Advantages2 from "./(home)/_components/Advantages2";
+// import Carousel from "./(home)/_components/Carousel";
 import Courses from "./(home)/_components/Courses";
 import Faq from "./(home)/_components/Faq";
 import HeroSection from "./(home)/_components/HeroSection";
@@ -15,8 +15,8 @@ export default function Home() {
 			<HeroSection />
 			<Advantages />
 			<About />
-			<Advantages2 />
-			<Carousel />
+			{/* <Advantages2 /> */}
+			{/* <Carousel /> */}
 			<Services />
 			<Courses />
 			<KnowledgeBase />
