718b2430ce15edaa2be2ee90fd9928e3f9258ac6
DmitriiFRS <dmitriifrs@gmail.com>
2026-02-23 11:46:14 +0500

    upd

--- src/features/profile/model/useProfileForm.tsx
+++ src/features/profile/model/useProfileForm.tsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { profileSchema } from "./profileSchema";
 import { z } from "zod";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { UserData } from "@/entities/user";
 import { updateProfile } from "../api/updateProfile";
 import { toast } from "react-toastify";
@@ -15,23 +15,12 @@ export const useProfileForm = () => {
 	const { me } = useContext(UserData);
 	const form = useForm<ProfileFormValues>({
 		resolver: zodResolver(profileSchema),
-		defaultValues: {
+		values: {
 			name: me?.name || "",
 			email: me?.email || "",
 			phoneNumber: me?.phoneNumber || "",
 		},
 	});
-
-	useEffect(() => {
-		if (me) {
-			console.log(me);
-			form.reset({
-				name: me.name || "",
-				email: me.email || "",
-				phoneNumber: me.phoneNumber || "",
-			});
-		}
-	}, [me, form]);
 
 	const onSubmit = async (data: ProfileFormValues) => {
 		try {
