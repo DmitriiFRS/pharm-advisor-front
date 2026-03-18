55e3d5654671ce35db486511c5121c46a4de32ec
DmitriiFRS <dmitriifrs@gmail.com>
2026-03-19 00:19:42 +0500

    upd

--- src/features/faq/model/useFaqs.ts
+++ src/features/faq/model/useFaqs.ts
@@ -1,9 +1,11 @@
 import { useData } from "@/shared/api/hooks/useData";
 import { IFaq } from "../types/faq.types";
 import { FAQ_ENDPOINTS } from "../api/faq.endpoints";
+import { useLocale } from "next-intl";
 
 export const useFaqs = () => {
-	const { data, loading, error, refetch } = useData<{ data: IFaq[] }>(FAQ_ENDPOINTS.FAQS);
+	const locale = useLocale();
+	const { data, loading, error, refetch } = useData<{ data: IFaq[] }>(FAQ_ENDPOINTS.FAQS, {}, locale);
 
 	return {
 		faqs: data?.data || [],
--- src/features/services/model/useServices.ts
+++ src/features/services/model/useServices.ts
@@ -3,9 +3,11 @@
 import { useData } from "@/shared/api/hooks/useData";
 import { SERVICES_ENDPOINTS } from "../api/services.endpoints";
 import { IService } from "../types/services.types";
+import { useLocale } from "next-intl";
 
 export const useServices = () => {
-	const { data, loading, error, refetch } = useData<{ data: IService[] }>(SERVICES_ENDPOINTS.SERVICES);
+	const locale = useLocale();
+	const { data, loading, error, refetch } = useData<{ data: IService[] }>(SERVICES_ENDPOINTS.SERVICES, {}, locale);
 
 	return {
 		services: data?.data || [],
